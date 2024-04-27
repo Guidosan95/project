b51cb7183bc57991cc2bef252ad6ba901ea5b524
Guidosan95 <guidosan95@outlook.com>
2024-04-27 11:36:45 -0300

    Primer Commit

---
+++ Desarrollo web con HTML y CSS/Clase 5/Desafio 5/codigo.php
@@ -0,0 +1,14 @@
+<?php
+$name=$_POST['nombre'];
+$email=$_POST['correo'];
+$mensaje=$_POST['mensaje'];
+
+echo '
+    <p>Su mensaje a sido enviado</p>
+'
+
+
+
+
+?>
+
---
+++ Desarrollo web con HTML y CSS/Clase 5/Etapa 5/codigo.php
@@ -0,0 +1,9 @@
+<?php
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$mensaje=$_POST['mensaje'];
+
+echo' <p>Su mensaje ha sido enviado</p>'
+
+
+?>
